c676a9862c96e52ef7aef6745d152bcaccbd802d
Eric Haase <erichaase@gmail.com>
2020-12-23 10:26:01 -0600

    added request to get player stats

--- importer.go
+++ importer.go
@@ -11,16 +11,13 @@ import (
 )
 
 func main() {
-	ids := get_game_ids()
-	fmt.Println(ids)
+	// ids := get_started_game_ids()
+	// fmt.Println(ids)
+	print_game_stats(401266806)
 }
 
-type statusType struct {
-	State string // values: "pre", "in", "post"
-}
-
-type status struct {
-	Type statusType
+type scoreboard struct {
+	Events []event
 }
 
 type event struct {
@@ -28,12 +25,16 @@ type event struct {
 	Status status
 }
 
-type scoreboard struct {
-	Events []event
+type status struct {
+	Type statusType
+}
+
+type statusType struct {
+	State string // values: "pre", "in", "post"
 }
 
 // move into espn scoreboard api client
-func get_game_ids() []int {
+func get_started_game_ids() []int {
 	u := &url.URL{
 		Scheme: "http",
 		Host: "site.api.espn.com",
@@ -77,4 +78,88 @@ func get_game_ids() []int {
 	}
 
 	return ids
+}
+
+type game struct {
+	Gamecast gamecast
+}
+
+type gamecast struct {
+	Stats stats
+}
+
+type stats struct {
+	Player players
+}
+
+type players struct {
+	Home []player
+	Away []player
+}
+
+type player struct {
+	Id int
+	FirstName string
+	LastName string
+	PositionAbbrev string
+	Jersey string
+	Active string
+	IsStarter string
+	Fg string
+	Ft string
+	Threept string
+	Rebounds string
+	Assists string
+	Steals string
+	Fouls string
+	Points string
+	Minutes string
+	Blocks string
+	Turnovers string
+	PlusMinus string
+	Dnp bool
+	EnteredGame bool
+}
+
+func print_game_stats(gid int) {
+	query := fmt.Sprintf("xhr=1&gameId=%d&lang=en&init=true&setType=true&confId=null", gid)
+	u := &url.URL{
+		Scheme: "http",
+		Host: "scores.espn.go.com",
+		Path: "nba/gamecast12/master",
+		RawQuery: query,
+	}
+
+	// var myClient = &http.Client{Timeout: 10 * time.Second}
+	resp, err := http.Get(u.String())
+	// check for non successful http status
+	if err != nil { // shorthand to make this terse
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	// json.NewDecoder(r.Body).Decode(target)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+		// exit?
+	}
+
+	g := game{}
+	err = json.Unmarshal(body, &g)
+	if err != nil {
+		log.Fatalln(err)
+		// exit?
+	}
+
+	// skip totals player row
+	for _, p := range g.Gamecast.Stats.Player.Home {
+		fmt.Printf("%s %s: %s\n", p.FirstName, p.LastName, p.Points)
+	}
+
+	for _, p := range g.Gamecast.Stats.Player.Away {
+		fmt.Printf("%s %s: %s\n", p.FirstName, p.LastName, p.Points)
+	}
+
+    // build new player struct for my use
 }
