f49952db23a73db9b0abe1997e221ff44fa022d5
Eric Haase <erichaase@gmail.com>
2020-12-24 21:36:48 -0600

    updated z scores using personal values

--- importer.go
+++ importer.go
@@ -227,13 +227,15 @@ type GameLine struct {
 func mapEspnGameLines(egls []EspnGameLine) []GameLine {
 	var gls []GameLine
 	for _, egl := range egls {
-		gl := mapEspnGameLine(egl)
+		gl := buildGameLine(egl)
 		gls = append(gls, gl)
 	}
 	return gls
 }
 
-func mapEspnGameLine(egl EspnGameLine) GameLine {
+func buildGameLine(egl EspnGameLine) GameLine {
+	// populateStats()
+	// populateZScores()
 	min, _ := strconv.Atoi(egl.Minutes)
 	pts, _ := strconv.Atoi(egl.Points)
 	reb, _ := strconv.Atoi(egl.Rebounds)
@@ -249,23 +251,22 @@ func mapEspnGameLine(egl EspnGameLine) GameLine {
 	var tpm, tpa int
 	fmt.Sscanf(egl.Threept, "%d/%d", &tpm, &tpa)
 
-	// better way to calculate this?
-	// double check the following using hashtag
+	// details: https://www.reddit.com/r/fantasybball/comments/71bdq0/how_to_calculate_weighted_zscore_for_fg/
 	zfg := 0.0
 	if fga != 0 {
-		zfg = (((float64(fgm) / float64(fga)) - 0.457) / 0.06236986313) * (float64(fga) / (5.011649158 * 2))
+		zfg = ((((float64(fgm) / float64(fga)) - 0.478) * float64(fga)) + 0.07) / 0.63
 	}
 	zft := 0.0
 	if fta != 0 {
-		zft = (((float64(ftm) / float64(fta)) - 0.775) / 0.08132524135) * (float64(fta) / (1.819730344 * 2))
+		zft = ((((float64(ftm) / float64(fta)) - 0.780) * float64(fta)) - 0.02) / 0.33
 	}
-	ztp := (float64(tpm) - 1.5) / 0.9
-	zpts := (float64(pts) - 13.1) / 6.35
-	zreb := (float64(reb) - 4.8) / 2.4
-	zast := (float64(ast) - 2.5) / 1.8
-	zstl := (float64(stl) - 0.9) / 0.4
-	zblk := (float64(blk) - 0.5) / 0.5
-	zto := -((float64(to) - 1.55) / 0.85)
+	ztp := (float64(tpm) - 1.69) / 1.01
+	zpts := (float64(pts) - 16.29) / 5.88
+	zreb := (float64(reb) - 6.21) / 2.58
+	zast := (float64(ast) - 3.52) / 2.17
+	zstl := (float64(stl) - 1.00) / 0.36
+	zblk := (float64(blk) - 0.71) / 0.52
+	zto := -((float64(to) - 1.95) / 0.87)
 	zsum := zfg + zft + ztp + zpts + zreb + zast + zstl + zblk + zto
 
 	return GameLine{
